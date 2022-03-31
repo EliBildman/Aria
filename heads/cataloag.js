c9a01f3c0e6ab66b4154b96a8977fcf58aeb9481
EliBildman <esbildman@gmail.com>
2022-03-31 15:50:15 -0500

    added email head

--- heads/cataloag.js
+++ heads/cataloag.js
@@ -3,29 +3,24 @@ const TP_Plug = require('./tp-plug');
 const Motion_Sensor = require('./motion-sensor');
 const Util = require('./util');
 const Scripts = require('./scripts');
+const Email = require('./email');
 
-const heads = [
-    TP_Plug,
-    Motion_Sensor,
-    Util,
-    Scripts,
-]
+const heads = [TP_Plug, Motion_Sensor, Util, Scripts, Email];
 
 //maybe temporary, need a list of actions / events
 const actions = {};
 const events = [];
-for(let head of heads) {
-    for(let action of head.actions) {
-        actions[`${head.name}.${action.name}`] = action
+for (let head of heads) {
+    for (let action of head.actions) {
+        actions[`${head.name}.${action.name}`] = action;
     }
-    for(let event of head.events) {
-        events.push(`${head.name}.${event}`)
+    for (let event of head.events) {
+        events.push(`${head.name}.${event}`);
     }
 }
-
 
 module.exports = {
     heads,
     actions,
     events,
-}
+};
---
+++ heads/email.js
@@ -0,0 +1,65 @@
+var nodemailer = require('nodemailer');
+
+let transporter;
+
+const initialize = () => {
+    transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'elibotman@gmail.com',
+            pass: 'thebotman123',
+        },
+    });
+};
+
+const send_email = (to, contents, subject) => {
+    var mail_options = {
+        from: 'elibotman@gmail.com',
+        to,
+        subject,
+        text: contents,
+    };
+
+    transporter.sendMail(mail_options, (error, _info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(`[EMAIL]: Sent ${subject}`);
+        }
+    });
+};
+
+const Send = {
+    name: 'Send',
+    param: {
+        to: '*string',
+        '?contents': '*string',
+        '?field': '*string',
+        '?subject': '*string',
+    },
+    run: (payload, param) => {
+        let subject = `Aria Message ${new Date().toISOString()}`;
+        if (param.subject !== undefined) {
+            subject = param.subject;
+        }
+
+        let contents = '';
+        if (param.contents !== undefined) {
+            contents = param.contents;
+        }
+        if (param.field !== undefined) {
+            contents = payload[param.field];
+        }
+
+        send_email(param.to, contents, param.subject);
+
+        return payload;
+    },
+};
+
+module.exports = {
+    name: 'Email',
+    initialize,
+    events: [],
+    actions: [Send],
+};
