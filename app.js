f556c6edc47285b30a55b845a7fa10ab251c4c50
EliBildman <esbildman@gmail.com>
2022-03-31 15:58:13 -0500

    removed system triggers

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const socketio = require("socket.io");
-const cors = require("cors");
+const socketio = require('socket.io');
+const cors = require('cors');
 
 //managers
-const schedule_manager = require("./managers/schedule-manager");
-const event_manager = require("./managers/event-manager");
+const schedule_manager = require('./managers/schedule-manager');
+const event_manager = require('./managers/event-manager');
 
 //initialize user events and schedules
 schedule_manager.initialize_schedules();
 
 event_manager.initialize_events();
 event_manager.load_head_events(); //load in events served by heads
-event_manager.initialize_system_triggers(); //initialize system triggers
 
 //heads
-const heads = require("./heads/cataloag");
+const heads = require('./heads/cataloag');
 
 //initialize controlers
 heads.heads.forEach((head) => {
-  head.initialize();
+    head.initialize();
 });
 
 const port = 3000;
 
 //middleware
-const http_event = require("./middlewares/http-event");
-const http_log = require("./middlewares/http-log");
-const fileUpload = require("express-fileupload");
+const http_event = require('./middlewares/http-event');
+const http_log = require('./middlewares/http-log');
+const fileUpload = require('express-fileupload');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
 
 const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -48,24 +47,24 @@ const http_server = app.listen(port, () => console.log(`listening on ${port}`));
 const io = new socketio.Server(http_server);
 
 //routes
-const schedule_router = require("./routes/schedule-router");
-const routine_router = require("./routes/routine-router");
-const event_router = require("./routes/event-router");
-const action_router = require("./routes/action-router");
-const script_router = require("./routes/script-router");
+const schedule_router = require('./routes/schedule-router');
+const routine_router = require('./routes/routine-router');
+const event_router = require('./routes/event-router');
+const action_router = require('./routes/action-router');
+const script_router = require('./routes/script-router');
 
-app.use("/schedules", schedule_router);
-app.use("/routines", routine_router);
-app.use("/events", event_router);
-app.use("/actions", action_router);
-app.use("/scripts", script_router);
+app.use('/schedules', schedule_router);
+app.use('/routines', routine_router);
+app.use('/events', event_router);
+app.use('/actions', action_router);
+app.use('/scripts', script_router);
 
-const io_socket = require("./routes/io-socket");
+const io_socket = require('./routes/io-socket');
 io_socket(io);
 
 //index
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 // io_server.on('connection', (socket) => {
--- managers/event-manager.js
+++ managers/event-manager.js
@@ -1,115 +1,106 @@
-const fs = require("fs");
-const events = require("../events/events");
-const routine_manager = require("./routine-manager");
-const { v4: uuidv4 } = require("uuid");
-const head_catalog = require("../heads/cataloag");
+const fs = require('fs');
+const events = require('../events/events');
+const routine_manager = require('./routine-manager');
+const { v4: uuidv4 } = require('uuid');
+const head_catalog = require('../heads/cataloag');
 
-const events_path = "data/configs/events.json";
-
-const system_triggers = "events/system-triggers";
-const trigger_folder_rel = "../events/system-triggers";
+const events_path = 'data/configs/events.json';
 
 let event_listeners = [];
 
 module.exports.register_event = (event) => {
-  const callback = async () => {
-    for (routine of event.routines) {
-      await routine_manager.run_routine(routine.ID, { event_name: event.name });
-    }
-  };
+    const callback = async () => {
+        for (routine of event.routines) {
+            await routine_manager.run_routine(routine.ID, {
+                event_name: event.name,
+            });
+        }
+    };
 
-  event_listeners.push(events.on(event.name, callback));
+    event_listeners.push(events.on(event.name, callback));
 };
 
 module.exports.load_head_events = () => {
-  const saved_events = this.get_events();
+    const saved_events = this.get_events();
 
-  for (let event_name of head_catalog.events) {
-    if (!saved_events.some((event) => event.name === event_name)) {
-      this.create_event(
-        {
-          name: event_name,
-          routines: [],
-        },
-        true
-      );
+    for (let event_name of head_catalog.events) {
+        if (!saved_events.some((event) => event.name === event_name)) {
+            this.create_event(
+                {
+                    name: event_name,
+                    routines: [],
+                },
+                true
+            );
+        }
     }
-  }
 };
 
 module.exports.initialize_events = () => {
-  event_listeners.forEach((listener) => listener.cancel());
-  event_listeners = [];
+    event_listeners.forEach((listener) => listener.cancel());
+    event_listeners = [];
 
-  const saved_events = this.get_events();
+    const saved_events = this.get_events();
 
-  for (let _event of saved_events) {
-    this.register_event(_event);
-  }
+    for (let _event of saved_events) {
+        this.register_event(_event);
+    }
 };
 
 module.exports.prune_routine = (routine_ID) => {
-  const saved_events = this.get_events();
+    const saved_events = this.get_events();
 
-  for (let _event of saved_events) {
-    const new_routines = [];
-    for (routine of _event.routines) {
-      if (routine.ID != routine_ID) new_routines.push(routine);
+    for (let _event of saved_events) {
+        const new_routines = [];
+        for (routine of _event.routines) {
+            if (routine.ID != routine_ID) new_routines.push(routine);
+        }
+        _event.routines = new_routines;
     }
-    _event.routines = new_routines;
-  }
 
-  save_events(saved_events);
+    save_events(saved_events);
 };
 
 module.exports.get_events = () => {
-  return JSON.parse(fs.readFileSync(events_path));
+    return JSON.parse(fs.readFileSync(events_path));
 };
 
 const save_events = (events) => {
-  fs.writeFileSync(events_path, JSON.stringify(events));
-  this.initialize_events();
+    fs.writeFileSync(events_path, JSON.stringify(events));
+    this.initialize_events();
 };
 
 module.exports.create_event = (new_event, permenant) => {
-  const saved_events = this.get_events();
-  new_event.ID = uuidv4();
-  new_event.permenant = permenant ?? false;
-  saved_events.push(new_event);
+    const saved_events = this.get_events();
+    new_event.ID = uuidv4();
+    new_event.permenant = permenant ?? false;
+    saved_events.push(new_event);
 
-  save_events(saved_events);
+    save_events(saved_events);
 };
 
 module.exports.update_event = (ID, updated_event) => {
-  const saved_events = this.get_events();
+    const saved_events = this.get_events();
 
-  updated_event.ID = ID;
-  const old_event_ind = saved_events.findIndex((ev) => ev.ID == ID);
-  saved_events[old_event_ind] = updated_event;
+    updated_event.ID = ID;
+    const old_event_ind = saved_events.findIndex((ev) => ev.ID == ID);
+    saved_events[old_event_ind] = updated_event;
 
-  save_events(saved_events);
+    save_events(saved_events);
 };
 
 module.exports.delete_event = (ID) => {
-  const saved_events = this.get_events();
+    const saved_events = this.get_events();
 
-  const del_ind = saved_events.findIndex((ev) => ev.ID == ID);
-  saved_events.splice(del_ind, 1);
+    const del_ind = saved_events.findIndex((ev) => ev.ID == ID);
+    saved_events.splice(del_ind, 1);
 
-  save_events(saved_events);
+    save_events(saved_events);
 };
 
 module.exports.run_event = (ID) => {
-  const saved_events = this.get_events();
+    const saved_events = this.get_events();
 
-  const name = saved_events.find((e) => e.ID == ID).name;
-  events.run(name, {}); //payload?
-};
-
-module.exports.initialize_system_triggers = () => {
-  fs.readdirSync(system_triggers).forEach((cont) => {
-    if (cont.includes(".js")) {
-      require("./" + trigger_folder_rel + "/" + cont).register();
-    }
-  });
+    const name = saved_events.find((e) => e.ID == ID).name;
+    events.run(name, {}); //payload?
 };
